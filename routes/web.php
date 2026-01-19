0fca45ee7d3e063245e5cbe48caa8cdfc8c08bfc
Dika yonanda putra <yonandaputra05@gmail.com>
2026-01-19 10:30:47 +0700

    add: define routes for Contact, Faq, and Terms pages

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\FaqController;
+use App\Http\Controllers\TermsController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::get('/faq', [FaqController::class, 'index'])->name('faq');
+Route::get('/terms', [TermsController::class, 'index'])->name('terms');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
