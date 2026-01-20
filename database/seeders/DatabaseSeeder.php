fea5a828718954c6b051ac7ebb999b2266852d3d
Dika yonanda putra <yonandaputra05@gmail.com>
2026-01-20 13:53:22 +0700

    add: UserSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
 use Database\Seeders\BookingSeeder;
 use Database\Seeders\RolePermissionSeeder;
 
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
+            UserSeeder::class,
             BookingSeeder::class,
             RolePermissionSeeder::class,
         ]);
