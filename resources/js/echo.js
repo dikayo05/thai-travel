7ad2a347762d2d8b20e8284d87a46cdcdc323ebc
Dika yonanda putra <yonandaputra05@gmail.com>
2026-02-06 09:48:11 +0700

    add: csrf token

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,27 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
+const csrfToken = document
+    .querySelector('meta[name="csrf-token"]')
+    ?.getAttribute("content");
+
 window.Echo = new Echo({
-    broadcaster: 'reverb',
+    broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+    enabledTransports: ["ws", "wss"],
+    authEndpoint: "/broadcasting/auth",
+    withCredentials: true,
+    auth: {
+        headers: csrfToken
+            ? {
+                  "X-CSRF-TOKEN": csrfToken,
+              }
+            : {},
+    },
 });
